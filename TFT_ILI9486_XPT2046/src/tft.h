ee0ebd8fd34c555d87842850417e81295b2f4771
Wolle <schreibfaul1@t-online.de>
2017-11-06 19:14:34 +0100

    correct color definitions

--- TFT_ILI9486_XPT2046/src/tft.h
+++ TFT_ILI9486_XPT2046/src/tft.h
@@ -31,26 +31,46 @@ extern __attribute__((weak)) void tp_released();
 #define ILI9486_WDBVAL  0x51 // Write Display Brightness Value
 #define ILI9486_CDBVAL  0x53 // Write Control Display Value
 
-// Color definitions
-#define TFT_BLACK       0x0000 /*   0,   0,   0 */
-#define TFT_NAVY        0x000F /*   0,   0, 128 */
-#define TFT_DARKGREEN   0x03E0 /*   0, 128,   0 */
-#define TFT_DARKCYAN    0x03EF /*   0, 128, 128 */
-#define TFT_MAROON      0x7800 /* 128,   0,   0 */
-#define TFT_PURPLE      0x780F /* 128,   0, 128 */
-#define TFT_OLIVE       0x7BE0 /* 128, 128,   0 */
-#define TFT_LIGHTGREY   0xC618 /* 192, 192, 192 */
-#define TFT_DARKGREY    0x7BEF /* 128, 128, 128 */
-#define TFT_BLUE        0x001F /*   0,   0, 255 */
-#define TFT_GREEN       0x07E0 /*   0, 255,   0 */
-#define TFT_CYAN        0x07FF /*   0, 255, 255 */
-#define TFT_RED         0xF800 /* 255,   0,   0 */
-#define TFT_MAGENTA     0xF81F /* 255,   0, 255 */
-#define TFT_YELLOW      0xFFE0 /* 255, 255,   0 */
-#define TFT_WHITE       0xFFFF /* 255, 255, 255 */
-#define TFT_ORANGE      0xFD20 /* 255, 165,   0 */
-#define TFT_GREENYELLOW 0xAFE5 /* 173, 255,  47 */
-#define TFT_PINK        0xF81F
+// RGB565 Color definitions
+#define TFT_AQUAMARINE      0x7FFA // 127, 255, 212
+#define TFT_BEIGE           0xF7BB // 245, 245, 220
+#define TFT_BLACK           0x0000 //   0,   0,   0
+#define TFT_BLUE            0x001F //   0,   0, 255
+#define TFT_BROWN           0xA145 // 165,  42,  42
+#define TFT_CHOCOLATE       0xD343 // 210, 105,  30
+#define TFT_CORNSILK        0xFFDB // 255, 248, 220
+#define TFT_CYAN            0x07FF //   0, 255, 255
+#define TFT_DARKGREEN       0x0320 //   0, 100,   0
+#define TFT_DARKGREY        0xAD55 // 169, 169, 169
+#define TFT_DARKCYAN        0x0451 //   0, 139, 139
+#define TFT_DEEPSKYBLUE     0x05FF //   0, 191, 255
+#define TFT_GRAY            0x8410 // 128, 128, 128
+#define TFT_GREEN           0x0400 //   0, 128,   0
+#define TFT_GREENYELLOW     0xAFE5 // 173, 255,  47
+#define TFT_GOLD            0xFEA0 // 255, 215,   0
+#define TFT_HOTPINK         0xFB56 // 255, 105, 180
+#define TFT_LAVENDER        0xE73F // 230, 230, 250
+#define TFT_LAWNGREEN       0x7FE0 // 124, 252,   0
+#define TFT_LIGHTBLUE       0xAEDC // 173, 216, 230
+#define TFT_LIGHTCYAN       0xE7FF // 224, 255, 255
+#define TFT_LIGHTGREY       0xD69A // 211, 211, 211
+#define TFT_LIGHTGREEN      0x9772 // 144, 238, 144
+#define TFT_LIGHTYELLOW     0xFFFC // 255, 255, 224
+#define TFT_LIME            0x07E0 //   0. 255,   0
+#define TFT_MAGENTA         0xF81F // 255,   0, 255
+#define TFT_MAROON          0x7800 // 128,   0,   0
+#define TFT_MEDIUMVIOLETRED 0xC0B0 // 199,  21, 133
+#define TFT_NAVY            0x000F //   0,   0, 128
+#define TFT_OLIVE           0x7BE0 // 128, 128,   0
+#define TFT_ORANGE          0xFD20 // 255, 165,   0
+#define TFT_PINK            0xFE19 // 255, 192, 203
+#define TFT_PURPLE          0x780F // 128,   0, 128
+#define TFT_RED             0xF800 // 255,   0,   0
+#define TFT_SANDYBROWN      0xF52C // 244, 164,  96
+#define TFT_TURQUOISE       0x471A //  64, 224, 208
+#define TFT_VIOLET          0x801F // 128,   0, 255
+#define TFT_WHITE           0xFFFF // 255, 255, 255
+#define TFT_YELLOW          0xFFE0 // 255, 255,   0
 
 
 
