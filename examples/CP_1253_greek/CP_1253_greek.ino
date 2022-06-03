16c0e9ebc4581b02b10c26f5b785734a2fdae26b
Wolle <schreibfaul1@t-online.de>
2022-06-03 13:45:52 +0200

    Update CP_1253_greek.ino

--- examples/CP_1253_greek/CP_1253_greek.ino
+++ examples/CP_1253_greek/CP_1253_greek.ino
@@ -1,21 +1,16 @@
-/*
- * Example for codepage 1253 greek
- *
- *
- * uncomment #include "fonts/Garamond_greek.h" in tft.h
- */
-
 #include "Arduino.h"
-#include "SPI.h"
 #include "ili9486.h"
 
-#define TFT_CS        22    // do not use GPI032 or GPIO33 here
-#define TFT_DC        21    // do not use GPI032 or GPIO33 here
+// GPIOs for SPI
 #define SPI_MOSI      23
 #define SPI_MISO      19
 #define SPI_SCK       18
-#define TP_CS         16
 
+// GPIOs for TFT/TP
+#define TFT_CS        22
+#define TFT_DC         5
+#define TP_CS         13
+#define TP_IRQ        12
 
 TFT tft;
 
@@ -23,14 +18,13 @@ void setup() {
     String txt="";
     pinMode(TP_CS, OUTPUT);
     digitalWrite(TP_CS, HIGH); // disable the touchpad
-    SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI);
-    tft.begin(TFT_CS, TFT_DC, SPI_MOSI, SPI_MISO, SPI_SCK);
+	tft.begin(TFT_CS, TFT_DC, VSPI, SPI_MOSI, SPI_MISO, SPI_SCK);
     tft.fillScreen(TFT_BLACK);
     tft.setRotation(1);
     tft.setCursor(0, 0);
-    tft.setFont(Garamond35x31greek);
+    tft.setFont(Times_New_Roman43x35);
     tft.setTextColor(TFT_YELLOW);
-    txt="Ο Παπάς πρώτα τα γένια του ευλογεί.";
+ 	txt="Ο Παπάς πρώτα τα γένια του ευλογεί.";
     tft.print(txt);
     tft.setTextColor(TFT_WHITE);
     txt=" The pastor blesses his own beard first\n";
@@ -41,7 +35,6 @@ void setup() {
     tft.setTextColor(TFT_WHITE);
     txt= " A drunk is more dangerous than a madman";
     tft.print(txt);
-
 }
 //-------------------------------------------------------------------------------------
 void loop(void) {
